4d0b268a95bcf309a1d2fab122ea30b2dda7e39f
Corneezius <cfr2116@columbia.edu>
2017-01-25 18:18:05 -0500

    correct responsive background image for mobile with full height vh

--- js/blink.js
+++ js/blink.js
@@ -1,12 +1,112 @@
 
 //
 $(document).ready(function() {
+	$('#rumble').hide(0).fadeIn(1000);
+	$('#bgvid').hide();
+	$('#bgvid')[0].play();
 
 	setTimeout(function(){
-    $('body').addClass('loaded');
-    $('.overlay_loader').hide();
-    $('.loader').hide();
-	}, 3000);
+		$('#rumble').fadeOut(1000);
+		// $('#ninety-six').fadeOut(1000);
+	}, 1500);
+
+
+	setTimeout(function(){
+		$('#bgvid')[0].play();
+	}, 2600);
+	//
+	// setTimeout(function(){
+	//
+	// 	$('#bgvid').hide();
+	// }, 6800);
+	// //
+	// setTimeout(function(){
+	// 	// $('video').addClass('premier');
+	// 		$('#bgvid').show();
+	// 	// $('#ninety-six').hide();
+	// }, 9000);
+	//
+	// setTimeout(function(){
+	// 	$('#corn').hide();
+	// }, 10500);
+	//
+	//
+	// setTimeout(function(){
+	// 	// $('video').addClass('premier');
+	// 	$('#bgvid2')[0].play();
+	// }, 10600);
+	//
+	// setTimeout(function(){
+	// 	$('#bgvid2').hide();
+	// }, 22500);
+	//
+	// setTimeout(function(){
+	// 		$('#chapter').show();
+	// 		$('#started').show();
+	// }, 22600);
+	//
+	// setTimeout(function(){
+	// 	$('#mainNav').show();
+	// 	$('#chapter').hide();
+	// 	$('#started').hide();
+	// 	// $('#corner').show();
+	// }, 24000);
+	//
+	// setTimeout(function(){
+	// 	$('body').addClass('loaded');
+	// 	$('body').removeClass("noscroll");
+	//
+	// 	// $('#corner').show();
+	// }, 24500);
+
+
+
+
+	//
+	// $('#name').hide(0).fadeIn(1000);
+	//
+	// setTimeout(function(){
+	// 	$('#rumble').fadeOut(1000);
+	// }, 1500);
+	//
+	// setTimeout(function(){
+	// 	$('.overlay_loader').show();
+	// 	$('.loader').show();
+	// }, 3000);
+	//
+	// setTimeout(function(){
+	// 	$('#memphis').show();
+	// }, 2000);
+	//
+	// setTimeout(function(){
+	// 	$('#memphis').hide();
+	// 	$('#ninety-six').show();
+	// }, 2000);
+
+	//
+
+	// setTimeout(function(){
+	// 	$('.overlay_loader').hide();
+	// 	$('#ninety-six').hide();
+	// 	$('video').addClass('premier');
+	// 	$('#bgvid')[0].play();
+	// }, 7000);
+	//
+    // setTimeout(function(){
+    //   $('body').addClass('loaded');
+		// 	$('.load-title').removeClass("load-title");
+		// 	$('#credit').hide();
+		// 	$('#bars').hide();
+		// 	$('.title').removeClass("clear");
+		// 	$('body').removeClass("noscroll");
+    // }, 18000);
+		//
+		//   setTimeout(function(){
+		// 		  $('body').addClass('.loaded');
+		// 			$('body').removeClass("noscroll");
+		// 		}, 6400);
+
+
 
 
 });
---
+++ js/video.js
@@ -0,0 +1,6 @@
+// var video = document.getElementById("#bgvid");
+// video.addEventListener("canplay", function() {
+//   setTimeout(function() {
+//     video.play();
+//   }, 5000);
+// });
