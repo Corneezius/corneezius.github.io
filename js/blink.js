052459312780b236b05751a6762c507d0e96f105
Corneezius <cfr2116@columbia.edu>
2017-02-07 00:57:08 -0500

    bring preloader javscript back

--- js/blink.js
+++ js/blink.js
@@ -30,33 +30,33 @@ $(document).ready(function() {
 		$('#started').show();
 	}, 5950);
 
-	// setTimeout(function(){
-	// 	$('#mainNav').show();
-	// 	$('#chapter').hide();
-	// 	$('#started').hide();
-	// 	$('.btn-toggle').show();
-	// 	$('body').addClass('loaded');
-	// 	$('body').removeClass("noscroll");
-	// }, 8950);
-  //
-	// setTimeout(function(){
-	// 	$('#mainNav').show();
-	// 	$('.btn-toggle').show();
-	// }, 9000);
-  //
-	// $("#push").click(function(){
-	// 		$('html, body').animate({ scrollTop: 0 }, 0);
-	// 		$('#bgvid').show();
-	// 		$('.navbar-custom').css('background-color','black');
-	// 		$('header').css('background','black');
-	// 		$('#bgvid')[0].play();
-  //
-	// 		setTimeout(function(){
-	// 			$('#bgvid').fadeOut(500);
-	// 			$('header').css('background','');
-	// 			$('.navbar-custom').css('background-color', '');
-	// 			// $('#ninety-six').fadeOut(1000);
-	// 		}, 55000);
-	// })
+	setTimeout(function(){
+		$('#mainNav').show();
+		$('#chapter').hide();
+		$('#started').hide();
+		$('.btn-toggle').show();
+		$('body').addClass('loaded');
+		$('body').removeClass("noscroll");
+	}, 8950);
+
+	setTimeout(function(){
+		$('#mainNav').show();
+		$('.btn-toggle').show();
+	}, 9000);
+
+	$("#push").click(function(){
+			$('html, body').animate({ scrollTop: 0 }, 0);
+			$('#bgvid').show();
+			$('.navbar-custom').css('background-color','black');
+			$('header').css('background','black');
+			$('#bgvid')[0].play();
+
+			setTimeout(function(){
+				$('#bgvid').fadeOut(500);
+				$('header').css('background','');
+				$('.navbar-custom').css('background-color', '');
+				// $('#ninety-six').fadeOut(1000);
+			}, 55000);
+	})
 
 });
