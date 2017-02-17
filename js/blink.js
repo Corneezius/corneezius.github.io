e34250dec67bf03c617f2254ba542a2423451935
Corneezius <cfr2116@columbia.edu>
2017-02-17 04:21:44 -0500

    add meta icon to home page

--- js/blink.js
+++ js/blink.js
@@ -47,18 +47,18 @@ $(document).ready(function() {
 	}, 9000);
 
 	$("#push").click(function(){
-			$('html, body').animate({ scrollTop: 0 }, 0);
-			$('#bgvid').show();
-			$('.navbar-custom').css('background-color','black');
-			$('header').css('background','black');
-			$('#bgvid')[0].play();
+		$('html, body').animate({ scrollTop: 0 }, 0);
+		$('#bgvid').show();
+		$('.navbar-custom').css('background-color','black');
+		$('header').css('background','black');
+		$('#bgvid')[0].play();
 
-			setTimeout(function(){
-				$('#bgvid').fadeOut(500);
-				$('header').css('background','');
-				$('.navbar-custom').css('background-color', '');
-				// $('#ninety-six').fadeOut(1000);
-			}, 55000);
+		setTimeout(function(){
+			$('#bgvid').fadeOut(500);
+			$('header').css('background','');
+			$('.navbar-custom').css('background-color', '');
+			// $('#ninety-six').fadeOut(1000);
+		}, 55000);
 	})
 
 });
