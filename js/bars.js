9eb5f7a1a4bba4d4a847eb3373666a06734dc1f2
Corneezius <cfr2116@columbia.edu>
2017-09-09 22:17:46 -0400

    update intro

--- js/bars.js
+++ js/bars.js
@@ -1,30 +1,29 @@
 
 $(document).ready(function() {
-	  $('.load-title').hide();
-		$('#name').hide(0).fadeIn(1000);
+		$('#rumble-credit').hide(0).fadeIn(1000);
 
-	setTimeout(function(){
-		$('#name').fadeOut(1000);
-	}, 1500);
-	//
-	setTimeout(function(){
-		$('.load-title').show();
-		$('#credit').show();
-		$('#bars').show();
-	}, 3000);
-	//
-  setTimeout(function(){
-		$('.load-title').removeClass("load-title");
-		$('#credit').hide();
-		$('#bars').hide();
-  }, 6000);
+		setTimeout(function(){
+			$('#rumble-credit').fadeOut(1000);
+		}, 1500);
+
+		setTimeout(function(){
+			$('#project-title').show();
+			$('#bars').show();
+			$('#artist-name').show();
+		}, 3000);
+
+	  setTimeout(function(){
+			$('#project-title').hide();
+			$('#bars').hide();
+			$('#artist-name').hide();
+	  }, 6000);
+
 		//
   setTimeout(function(){
 		  $('body').addClass('loaded');
 			$('body').removeClass("noscroll");
 		}, 6400);
-
-});
+ });
 
 $(window).on('beforeunload', function() {
     $(window).scrollTop(0);
@@ -43,12 +42,12 @@ $(window).scroll(function() {
     }
   });
 
-  $('#footer-fade').css({
-    opacity: function() {
-      var elementHeight = $(this).height(),
-          opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.78);
-
-      return opacity;
-    }
-  });
+  // $('#footer-fade').css({
+  //   opacity: function() {
+  //     var elementHeight = $(this).height(),
+  //         opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.78);
+	//
+  //     return opacity;
+  //   }
+  // });
 });
