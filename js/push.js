f8dadd647ddfe16c783456ecf675c7e1389d270d
Corneezius <cfr2116@columbia.edu>
2019-10-16 21:28:09 -0400

    update function with user ip

--- js/push.js
+++ js/push.js
@@ -1,50 +1,71 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyAzQ6nHNT0D8OsBDF5C3UMPLI3p8EKiDgo",
-  authDomain: "push-e0c02.firebaseapp.com",
-  databaseURL: "https://push-e0c02.firebaseio.com",
-  projectId: "push-e0c02",
-  storageBucket: "",
-  messagingSenderId: "572888864837",
-  appId: "1:572888864837:web:f031b37c199428c91fab2c",
-  measurementId: "G-30NDF7FVTW"
+	apiKey: 'AIzaSyAzQ6nHNT0D8OsBDF5C3UMPLI3p8EKiDgo',
+	authDomain: 'push-e0c02.firebaseapp.com',
+	databaseURL: 'https://push-e0c02.firebaseio.com',
+	projectId: 'push-e0c02',
+	storageBucket: '',
+	messagingSenderId: '572888864837',
+	appId: '1:572888864837:web:f031b37c199428c91fab2c',
+	measurementId: 'G-30NDF7FVTW'
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-
 // function to updateCount
 var push = document.getElementById('push');
-push.addEventListener('click', function push() {
+push.addEventListener('click', async function() {
+	getIp();
+});
 
-  var updateCount = firebase.database().ref('pushCount/count');
-  updateCount.transaction(function(theCount) {
-
-        console.log(theCount);
-        return theCount + 1;
-    });
-  // var updatePusher.firebase.database().ref('pushCount/pusher');
-
-console.log(getIp());
-}, false);
 // function to getIP address
-function getIp() {
-  var request = new XMLHttpRequest();
-  request.open('GET', 'https://api.ipify.org/?format=json', true);
+async function getIp() {
+	var request = new XMLHttpRequest();
+	request.open('GET', 'https://api.ipify.org/?format=json', true);
 
-  request.onload = function() {
-    if (this.status >= 200 && this.status < 400) {
-      // Success!
-      var data = JSON.parse(this.response);
-      console.log(data);
-    } else {
-      // We reached our target server, but it returned an error
-      console.log("error");
-    }
-  };
-  request.onerror = function() {
-    // There was a connection error of some sort
-  };
-  request.send();
+	request.onload = function() {
+		if (this.status >= 200 && this.status < 400) {
+			// Success!
+			var data = JSON.parse(this.response);
+			var updateCountRef = firebase.database().ref('pushCount/userData');
+			updateCountRef
+				.orderByChild('ip')
+				.equalTo(data.ip)
+				.once('value')
+				.then(function(snapshot) {
+					if (!snapshot.val()) {
 
+						updateCountRef.push({ count: 1, ip: data.ip });
+						return;
+                    }
+                    let countOfusers = Object.keys(snapshot.val()).length + 1;
+                    document.getElementById("userCount").innerHTML = "User Count: " + countOfusers ;
+					snapshot.forEach(function(data) {
+						let val = snapshot.val();
+						firebase
+							.database()
+							.ref('pushCount/userData/' + data.key)
+							.update({ count: val[data.key].count + 1, ip: val[data.key].ip });
+					});
+				})
+				.catch(function(reason) {
+					console.log(reason);
+					updateCountRef.push({ count: 1, ip: data.ip });
+				})
+				.finally(() => {
+					var updateCount = firebase.database().ref('pushCount/count');
+					updateCount.transaction(function(theCount) {
+						var counter = theCount + 1;
+						document.getElementById('count').innerHTML = 'count: ' + counter;
+						return counter;
+					});
+				});
+		} else {
+			console.log('error');
+		}
+	};
+	request.onerror = function() {
+		// There was a connection error of some sort
+	};
+	request.send();
 }
